0640164d240d739818791075745c0f4c8b8940cf
freeser <freeser@126.com>
2026-04-28 23:53:36 +0800

    浅拷贝

---
+++ src/main/java/com/fyfe/prototype/shallowCopy/Playlist2.java
@@ -0,0 +1,45 @@
+package com.fyfe.prototype.shallowCopy;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Playlist2 implements Serializable, Cloneable {
+    private Long id;
+    private String name;
+    private List<Song> songs = new ArrayList<>();
+
+    public Playlist2() {
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void add(Song song){
+        songs.add(song);
+    }
+
+    public Playlist2(Playlist2 sourcePlayList) {
+        this.id = sourcePlayList.getId();
+        this.name = sourcePlayList.getName();
+        this.songs = sourcePlayList.getSongs();
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Playlist2 playlist = new Playlist2();
+        playlist.setId(1L);
+        playlist.setName("杰伦");
+        playlist.add(new Song("稻香","杰伦"));
+        playlist.add(new Song("迷迭香","杰伦"));
+        playlist.add(new Song("七里香","杰伦"));
+
+        // 浅拷贝后的最喜爱的专辑
+        Playlist2 favouriteList = (Playlist2) playlist.clone();
+        System.out.println(favouriteList);
+    }
+}
---
+++ src/main/java/com/fyfe/prototype/shallowCopy/Song.java
@@ -0,0 +1,11 @@
+package com.fyfe.prototype.shallowCopy;
+
+public class Song {
+    String title;
+    String artist;
+
+    Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+}
